fedb32ba3c70e131499f2b05bab56a1b7f99a9f4
Ijeoma Lorretta A <135869466+LiaBfab@users.noreply.github.com>
2023-09-27 03:40:29 +0100

    Restructured the interface and made it responsive

--- script/app.js
+++ script/app.js
@@ -1,29 +1,3 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-  for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-  }
-  for (tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-  }
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
-}
-
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu() {
-  sidemenu.style.right = "0";
-}
-function closemenu() {
-  sidemenu.style.right = "-200px";
-}
-
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbylL8S04DIa7fBwRDsyPc6orBRGXmf4me4bOKlGfvF7ZhWGI35RSoRglpsANe97yRFv/exec";
-const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", (e) => {
